09b6d501e15b2b00db3d522502aa0fca37e83aec
wangyiming <wangyiming@wangyimingdeMacBook-Pro.local>
2018-12-29 15:45:40 +0800

    add new code

---
+++ cpp/CF1096A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int T;
+	long long x, y;
+	cin >> T;
+	while (T--)
+	{
+		cin >> x >> y;
+		cout << x << " " << 2 * x << endl;
+	}
+	return 0;
+}
