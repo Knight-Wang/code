d88ce31ba15c720b94ec9f0b82df2ebbf0fa2d2d
wangyiming <wangyiming@wangyimingdeMacBook-Pro.local>
2018-12-31 18:29:12 +0800

    small modifications

--- cpp/CF1091C.cpp
+++ cpp/CF1091C.cpp
@@ -6,21 +6,19 @@ int main()
 	ll n;
 	while (cin >> n)
 	{
-		unordered_set<ll> st;
+		vector<ll> v;
 		for (ll i = 1; i * i <= n; i++)
 		{
 			if (n % i == 0)
 			{
 				ll tmp = n / i;
-				st.insert((2 + (tmp - 1) * i) * tmp / 2);
+				v.push_back((2 + (tmp - 1) * i) * tmp / 2);
 				if (tmp != i)
 				{
-					st.insert((2 + (i - 1) * tmp) * i / 2);
+					v.push_back((2 + (i - 1) * tmp) * i / 2);
 				}
 			}
 		}
-		vector<ll> v;
-		for (auto it: st) v.push_back(it);
 		sort(v.begin(), v.end());
 		for (auto it: v) cout << it << " ";
 		cout << endl;
