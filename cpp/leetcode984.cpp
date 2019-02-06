3c4b08cf264912fce3b86fcc4c11dd460fdf6bc0
wangyiming <wangyiming@wangyimingdeMacBook-Pro.local>
2019-02-07 00:25:55 +0800

    fix format

--- cpp/leetcode984.cpp
+++ cpp/leetcode984.cpp
@@ -1,35 +1,26 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution
 {
 public:
     string strWithout3a3b(int A, int B)
     {
-    	string str = "";
-    	if (A == B)
-    	{
-    		for (int i = 0; i < A; i++) str += "ab";
-    	}
-    	else
-    	{
-    		int mx = max(A, B), mn = min(A, B);
-  			int s = mx - mn - 1;
-  			char p = 'a', q = 'b';
-  			if (B > A) swap(p, q);
-  			for (int i = 0; i < mn + 1; i++)
-  			{
-  				str += p;
-  				if (i < s) str += p;
-  				if (i != mn) str += q;
-  			}
-    	}
-    	return str;
+        string str = "";
+        if (A == B)
+        {
+            for (int i = 0; i < A; i++) str += "ab";
+        }
+        else
+        {
+            int mx = max(A, B), mn = min(A, B);
+            int s = mx - mn - 1;
+            char p = 'a', q = 'b';
+            if (B > A) swap(p, q);
+            for (int i = 0; i < mn + 1; i++)
+            {
+                str += p;
+                if (i < s) str += p;
+                if (i != mn) str += q;
+            }
+        }
+        return str;
     }
 };
-
-int main()
-{
-	cout << Solution().strWithout3a3b(100, 202) << endl;
-	return 0;
-}
