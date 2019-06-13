6dfd3b86583c2a91e93dc342e4704519d9df91fe
wangyiming <wangyiming@wangyimingdeMacBook-Pro.local>
2019-06-13 16:30:01 +0800

    fixed a bug

--- cpp/CF1136D.cpp
+++ cpp/CF1136D.cpp
@@ -18,6 +18,13 @@ int main()
             cin >> x >> y;
             v[y].push_back(x);
         }
+        for (int i = n; i >= 1; i--)
+        {
+            for (int j = 0; j < v[a[i]].size(); j++)
+            {
+                if (p[v[a[i]][j]] < i) cnt[v[a[i]][j]]++;
+            }
+        }
         vector<pair<int, int>> t;
         for (int i = 0; i < v[a[n]].size(); i++)
         {
@@ -25,18 +32,18 @@ int main()
             t.push_back(make_pair(p[to], to));
         }
         sort(t.begin(), t.end());
-        for (int i = n; i >= 1; i--)
-        {
-            for (int j = 0; j < v[i].size(); j++)
-            {
-                cnt[v[i][j]]++;
-            }
-        }
         int cur = n, ans = 0;
         for (int i = t.size() - 1; i >= 0; i--)
         {
             x = t[i].second;
-            if (cnt[x] >= cur - t[i].first) { ans++; cur--; }
+            if (cnt[x] == cur - t[i].first)
+            {
+                ans++; cur--;
+                for (int j = 0; j < v[x].size(); j++)
+                {
+                    int to = v[x][j]; cnt[to]--;
+                }
+            }
         }
         cout << ans << endl;
     }
