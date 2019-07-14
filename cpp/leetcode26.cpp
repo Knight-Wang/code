cac73a712fd072ffc8e1d0d3cbf783f77b5475b1
wangyiming <wangyiming@wangyimingdeMacBook-Pro.local>
2019-07-14 13:58:48 +0800

    modified the code

--- cpp/leetcode26.cpp
+++ cpp/leetcode26.cpp
@@ -1,10 +1,16 @@
-class Solution {
+class Solution
+{
 public:
-    int removeDuplicates(vector<int>& nums) 
+    int removeDuplicates(vector<int>& nums)
     {
-        if (!nums.size()) return 0;
-        vector<int>::iterator it = unique(nums.begin(), nums.end());
-        nums.erase(it, nums.end());
-        return nums.size();
+        if (nums.empty()) return 0;
+        int n = nums.size();
+        int i = 1, cur = 1;
+        while (i < n)
+        {
+            while (i < n && nums[i] == nums[i - 1]) i++;
+            if (i < n) { nums[cur] = nums[i]; i++; cur++; }
+        }
+        return cur;
     }
 };
