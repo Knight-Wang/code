cf318330be32ea6df004c18f5ee81a89961e20c1
wangyiming <wangyiming@wangyimingdeMacBook-Pro.local>
2019-07-21 23:34:40 +0800

    add new code

--- cpp/leetcode516.cpp
+++ cpp/leetcode516.cpp
@@ -1,20 +1,18 @@
-class Solution 
+class Solution
 {
 public:
-    int longestPalindromeSubseq(string s) 
+    int longestPalindromeSubseq(string s)
     {
+        if (s.empty()) return 0;
         int n = s.length();
-        int dp[1001][1001];
-        for (int i = 0; i < n; i++)
-        {
-            dp[i][i] = 1;
-            if (i != n - 1) dp[i][i + 1] = (s[i] == s[i + 1] ? 2 : 1);
-        }
+        vector<vector<int>> dp(n, vector<int>(n, 0));
         for (int i = n - 1; i >= 0; i--)
         {
-            for (int j = i + 2; j < n; j++)
+            for (int j = i; j < n; j++)
             {
-                if (s[i] == s[j]) dp[i][j] = dp[i + 1][j - 1] + 2;
+                if (i == j) dp[i][j] = 1;
+                else if (j == i + 1) dp[i][j] = (s[i] == s[j]) + 1;
+                else if (s[i] == s[j]) dp[i][j] = dp[i + 1][j - 1] + 2;
                 else dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
             }
         }
