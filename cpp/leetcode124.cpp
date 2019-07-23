3470a1c0dfdfb3d759b69a32562cfb81f29d472c
wangyiming <wangyiming@wangyimingdeMacBook-Pro.local>
2019-07-23 20:44:03 +0800

    modified the code

--- cpp/leetcode124.cpp
+++ cpp/leetcode124.cpp
@@ -1,45 +1,29 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-struct TreeNode
-{
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
-class Solution 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution
 {
 public:
-	int dfs(TreeNode* root, vector<int>& v)
-	{
-		if (root == NULL) return 0;
-		int l = dfs(root->left, v);
-		int r = dfs(root->right, v);
-		int ans = root->val, tmp = max(l, r);
-		if (tmp > 0) ans += tmp;
-		int sum = root->val;
-		if (l > 0) sum += l;
-		if (r > 0) sum += r;
-		v.push_back(sum);
-		return ans;
-	}
+    int dfs(TreeNode* root, int& maxn)
+    {
+        if (!root) return 0;
+        int x = dfs(root->left, maxn);
+        int y = dfs(root->right, maxn);
+        if (x < 0) x = 0;
+        if (y < 0) y = 0;
+        maxn = max(maxn, x + y + root->val);
+        return max(x, y) + root->val;
+    }
     int maxPathSum(TreeNode* root)
     {
-    	vector<int> v;
-		dfs(root, v);
-		return *max_element(v.begin(), v.end());
+        int maxn = INT_MIN;
+        dfs(root, maxn);
+        return maxn;
     }
 };
-
-int main()
-{
-	TreeNode* root = new TreeNode(-10);
-	root->left = new TreeNode(9);
-	root->right = new TreeNode(20);
-	root->right->left = new TreeNode(15);
-	root->right->right = new TreeNode(7);
-	cout << Solution().maxPathSum(root) << endl;
-	return 0;
-}
