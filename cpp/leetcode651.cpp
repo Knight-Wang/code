34514c16e1ebb65210900ed3615cc0205983367a
wangyiming <wangyiming@wangyimingdeMacBook-Pro.local>
2019-09-22 13:51:00 +0800

    add new code

---
+++ cpp/leetcode651.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    int maxA(int N)
+    {
+        vector<int> dp(N + 1, 0);
+        dp[1] = 1;
+        for (int i = 2; i <= N; i++)
+        {
+            dp[i] = dp[i - 1] + 1;
+            for (int j = 1; j < i; j++)
+            {
+                if (i - j < 3) continue;
+                dp[i] = max(dp[i], dp[j] * (i - j - 1));
+            }
+        }
+        return dp[N];
+    }
+};
