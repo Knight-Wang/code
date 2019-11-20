008736824c0493a7ce98fab2939e863b2bc3527e
wangyiming <wangyiming@wangyimingdeMacBook-Pro.local>
2019-11-20 20:46:16 +0800

    modified the code

--- cpp/leetcode1105.cpp
+++ cpp/leetcode1105.cpp
@@ -4,20 +4,20 @@ public:
     int minHeightShelves(vector<vector<int>>& books, int shelf_width)
     {
         int n = books.size();
-        vector<vector<int>> dp(n, vector<int>(2, INT_MAX));
-        dp[0][0] = dp[0][1] = books[0][1];
-        for (int i = 1; i < n; i++)
+        vector<int> dp(n + 1, 0);
+        dp[0] = 0; dp[1] = books[0][1];
+        for (int i = 2; i <= n; i++)
         {
-            dp[i][1] = min(dp[i - 1][0], dp[i - 1][1]) + books[i][1];
-            int sum = books[i][0], maxn = books[i][1];
-            for (int j = i - 1; j >= 0; j--)
+            dp[i] = dp[i - 1] + books[i - 1][1];
+            int sum = books[i - 1][0], maxn = books[i - 1][1];
+            for (int j = i - 1; j >= 1; j--)
             {
-                if (sum + books[j][0] > shelf_width) break;
-                maxn = max(maxn, books[j][1]);
-                dp[i][0] = min(dp[i][0], dp[j][1] - books[j][1] + maxn);
-                sum += books[j][0];
+                if (sum + books[j - 1][0] > shelf_width) break;
+                maxn = max(maxn, books[j - 1][1]);
+                dp[i] = min(dp[i], dp[j - 1] + maxn);
+                sum += books[j - 1][0];
             }
         }
-        return min(dp[n - 1][0], dp[n - 1][1]);
+        return dp[n];
     }
 };
